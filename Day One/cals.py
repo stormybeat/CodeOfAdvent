864c79104a5becda2c33d7e7116d91ea93784def
stormybeat <128640313+stormybeat@users.noreply.github.com>
2023-05-18 12:11:16 -0400

    Update cals.py

--- Day One/cals.py
+++ Day One/cals.py
@@ -1,13 +1,15 @@
 with open("./input.txt", "r") as handle:
     calTotalList = []
-    myList = handle.read().splitLines()
+    myList = handle.read().split("\n\n")
     for m in myList:
         elfsCals = m.split("\n")
         newList = []
         totalCals = 0
         for everything in elfsCals:
+            if not everything:
+                continue
             newList.append(int(everything))
-            for number in newList:
-                totalCals = totalCals+number
+        for number in newList:
+            totalCals = totalCals+number
         calTotalList.append(totalCals)
     print(sorted(calTotalList))
